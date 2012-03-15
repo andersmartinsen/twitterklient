8ee129976addba6adea9941abfc92014b6c6e078
Anders Martinsen <anders.martinsen@bekk.no>
2012-03-15 16:49:28 +0100

    [Kontaktlista] Lagt til javascript for kontaktlista.

--- www/javascripts/app.js
+++ www/javascripts/app.js
@@ -3,6 +3,10 @@
     this.searchField = $(params.searchField);
     this.tweets = $(params.tweets);
     this.searchButton = $(params.searchButton);
+    this.twitterUserSearchButton = $(params.twitterUserSearchButton);
+    this.searchUsernameField = $(params.searchUsernameField);
+	this.usernameResultPage = $(params.usernameResultPage);
+	this.user = $(params.user);
     this.geoSearchButton = $(params.geoSearchButton);
     this.resultsPage = $(params.resultsPage);
     this.playButton = $(params.playButton);
@@ -50,15 +54,39 @@
         }, captureError, {limit: 1});
       });
     },
-    renderTweets: function(tweets){
-      var self = this;
-      $.each(tweets, function(i, tweet) {
-        if(tweet.text !== undefined) {
-          var tweet_html = '<li><h1>Tweet</h1><p>' + tweet.text + '</p><\/li>';
-          self.tweets.append(tweet_html);
+	renderUser: function(user){
+		var self = this;
+		var user_html = '<img src="' + user.profile_image_url_https + '"/>'
+		user_html += '<p>Navn: ' + user.name + '</p>';
+		user_html += '<p>Følgere: ' + user.followers_count + '</p>'
+		user_html += '<p>Følger: ' + user.friends_count + '</p>'
+		user_html += '<a href="" onClick="lagreAnsattTilKontaktLista(); return false;" class="ui-btn-right" data-role="button" data-icon="check">Save</a>';
+		self.user.append(user_html);
+    },
+	lagreAnsattTilKontaktLista: function() {
+		alert('her');
+        var contact = navigator.contacts.create();
+        contact.displayName = user.name;
+
+        var bilder = [1];
+        bilder[0] = new ContactField('Jobb', user.profile_image_url_https, true);
+        contact.photos = bilder;
+
+        var name = new ContactName();
+        name.formatted = user.name;
+        name.givenName = user.name;
+        name.familyName = user.name;
+        contact.name = name;
+
+        contact.save(onSuccess, onError);
+
+        function onSuccess() {
+            navigator.notification.alert('Kontakten ble lagret i adresseboka', null, 'Suksess');
         }
-      });
-      self.tweets.listview("refresh");
+
+        function onError(contactError) {
+            navigator.notification.alert('Feilkode: ' + contactError.code, null, 'En feil inntraff');
+        }
     },
     searchByLocation: function(){
       var self = this;
@@ -78,6 +106,26 @@
         });
       } , function(error){console.log("Something went wrong with location")});
     },
+	searchByTwitterName: function(username) {
+		console.log("Searching for " + username);
+	      var searchUrl = 'http://api.twitter.com/1/users/show.json?callback=?&screen_name=' + username;
+	      var self = this;
+	      $.ajax({
+	        url: searchUrl,
+	        dataType: "JSON",
+	        type: "GET",
+	        success: function(data) {
+	          $.mobile.changePage(self.usernameResultPage);
+	          self.renderUser(data); 
+	        },
+	        error: function(xhr, textStatus, errorThrown){
+	          console.log(xhr);
+	          console.log(textStatus);
+	          console.log(errorThrown);
+	          alert("Noe gikk galt når vi søkte etter bruker på twitter :(");
+	        }
+	      });
+	},
     search: function(keyword){
       console.log("Searching for " + keyword);
       var searchUrl = 'http://search.twitter.com/search.json?callback=?&q=' + keyword;
